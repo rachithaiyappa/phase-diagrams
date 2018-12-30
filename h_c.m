35a50ad6ce40423eb8cddc19579bb73594acb522
Rachith Aiyappa <44749902+rachithaiyappa@users.noreply.github.com>
2018-12-30 20:32:49 +0530

    The main code for the phase diagrams

---
+++ h_c.m
@@ -0,0 +1,63 @@
+function [] = h_c(s,N)
+
+% % c = linspace(0.001,1,100);
+% % h = linspace(0.01,0.1,100);
+% % s = 0.1;
+% % N = 50;
+% 
+% x_0 = (4.*N.*s.*(c+h))./(((2.*c)+h).^2);
+% x = x_0 - 1;
+% 
+% % y = h*m*m*N*0.5/((2*c)+h)
+%  
+% P = @(m,s,c,h,N) (((4.*s)+(((2.*c)+h).*(1-m.^2))).^x).*exp(h.*m.*m.*N.*0.5./((2.*c)+h));
+% I = integral(@(m)P(m,s,c,h,N),-1,1,'ArrayValued',true);
+% 
+% P0 = I;
+% 
+% Pf = @(m,s,c,h,N,P0) ((((4.*s)+(((2.*c)+h).*(1-m.^2))).^x).*exp(h.*m.*m.*N.*0.5/((2.*c)+h))./P0);
+% If = integral(@(m)2*m*Pf(m,s,c,h,N,P0),0,1,'ArrayValued',true);
+% 
+% 
+% fileID = fopen('h_c.txt','w');
+% for i = 1:1:size(h,2)
+%     h(i);
+%     fprintf(fileID,'%f \t %f \t %f\n',h(i),c(i),If(i));
+% end
+
+c = linspace(0.001,1000,50);
+h = linspace(0.001,1000,50);
+
+% s = 0.0001;
+% N = 50;
+
+[C,H] = meshgrid(c,h);
+
+x_0 = (4.*N.*s.*(C+H))./(((2.*C)+H).^2);
+x_0_prime = (4.*N.*s.*(C+H))./(((2.*C)+H).^2);
+x = x_0 - 1;
+
+y = H.*N.*0.5./((2.*C)+H);
+
+C(1,:)
+H(1,:)
+C(2,:)
+H(2,:)
+fileID = fopen('H_C.txt','w');
+% for i = 1:1:size(c,2)
+%     P = @(m,s,C,H,N) (((4.*s)+(((2.*C(i,:))+H(i,:)).*(1-m.^2))).^x).*exp(y.*m.*m)
+
+P = @(m,s,C,H,N) (((4.*s)+(((2.*C)+H).*(1-m.^2))).^x).*exp(y.*m.*m);
+I = integral(@(m)P(m,s,C,H,N),-1,1,'ArrayValued',true);
+
+P0 = I;
+% Pf = @(m,s,C,H,N,P0) ((((4.*s)+(((2.*C(i,:))+H(i,:)).*(1-m.^2))).^x).*exp(y.*m.*m)./P0);
+Pf = @(m,s,C,H,N,P0) ((((4.*s)+(((2.*C)+H).*(1-m.^2))).^x).*exp(y.*m.*m)./P0);
+If = integral(@(m)2*m*Pf(m,s,C,H,N,P0),0,1,'ArrayValued',true);
+%     for j = 1:1:size(h,2)
+%         fprintf(fileID,'%f \t %f \t %f\n',H(i,j),c(i,j),If(i));
+%     end
+% end
+
+surf(C,H,If);colorbar
+fclose(fileID)
