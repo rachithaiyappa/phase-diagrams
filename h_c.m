a29c45614b78ceda8e8da465372f3129f3f0b1f4
Rachith Aiyappa <44749902+rachithaiyappa@users.noreply.github.com>
2018-12-30 21:10:57 +0530

    Added few comments to the code

--- h_c.m
+++ h_c.m
@@ -25,7 +25,7 @@ function [] = h_c(s,N)
 %     fprintf(fileID,'%f \t %f \t %f\n',h(i),c(i),If(i));
 % end
 
-c = linspace(0.001,1000,50);
+c = linspace(0.001,1000,50); %setting axes range
 h = linspace(0.001,1000,50);
 
 % s = 0.0001;
@@ -33,7 +33,7 @@ h = linspace(0.001,1000,50);
 
 [C,H] = meshgrid(c,h);
 
-x_0 = (4.*N.*s.*(C+H))./(((2.*C)+H).^2);
+x_0 = (4.*N.*s.*(C+H))./(((2.*C)+H).^2); %Maing the PDF more readable by using compact forms
 x_0_prime = (4.*N.*s.*(C+H))./(((2.*C)+H).^2);
 x = x_0 - 1;
 
@@ -50,10 +50,10 @@ fileID = fopen('H_C.txt','w');
 P = @(m,s,C,H,N) (((4.*s)+(((2.*C)+H).*(1-m.^2))).^x).*exp(y.*m.*m);
 I = integral(@(m)P(m,s,C,H,N),-1,1,'ArrayValued',true);
 
-P0 = I;
+P0 = I; %obtaining the value of P_0, the normalisation constant 
 % Pf = @(m,s,C,H,N,P0) ((((4.*s)+(((2.*C(i,:))+H(i,:)).*(1-m.^2))).^x).*exp(y.*m.*m)./P0);
 Pf = @(m,s,C,H,N,P0) ((((4.*s)+(((2.*C)+H).*(1-m.^2))).^x).*exp(y.*m.*m)./P0);
-If = integral(@(m)2*m*Pf(m,s,C,H,N,P0),0,1,'ArrayValued',true);
+If = integral(@(m)2*m*Pf(m,s,C,H,N,P0),0,1,'ArrayValued',true); %using the value of P_0 to obtain the expecation value
 %     for j = 1:1:size(h,2)
 %         fprintf(fileID,'%f \t %f \t %f\n',H(i,j),c(i,j),If(i));
 %     end
